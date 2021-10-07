fdaacbb50b5c00532b51aa39c8405a9abcec7dbd
Niall McGuinness <niall.mcguinness@dkit.ie>
2021-10-07 11:53:50 +0100

    Add project files.

---
+++ GD3_3_Intro_To_MonoGame/Game1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GD3_3_Intro_To_MonoGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 640;
+            _graphics.PreferredBackBufferHeight = 480;
+            _graphics.ApplyChanges();
+
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+
+            System.Diagnostics.Debug.WriteLine("Game1");
+        }
+
+        protected override void Initialize()
+        {
+            System.Diagnostics.Debug.WriteLine("Initialize");
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            System.Diagnostics.Debug.WriteLine("LoadContent");
+        }
+
+        protected override void UnloadContent()
+        {
+            System.Diagnostics.Debug.WriteLine("UnloadContent");
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            System.Diagnostics.Debug.WriteLine("Update");
+            System.Diagnostics.Debug.WriteLine(gameTime.ElapsedGameTime.TotalMilliseconds);
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Red);
+
+            System.Diagnostics.Debug.WriteLine("Draw");
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ GD3_3_Intro_To_MonoGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GD3_3_Intro_To_MonoGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
